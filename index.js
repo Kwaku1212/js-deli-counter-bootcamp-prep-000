a46a938a2de8b2c5ed76f3da1d13e1b64804bf36
Kwaku J <gyazi@live.com>
2018-04-13 22:53:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 
-function takeANumber(katzDeliLine, name){
-  var katzDeli = []
-  for(var i = 0; i < katzDeliLine.length; i++){
-    var results = (`Welcome, ${name[i]}. You are number ${katzDeliLine[i]} in line.`)
-    return results
-  }
-}
